36ed56857c65580412fe5ec7f6dd2605ec5f9d37
Matthew Jones <matts3371@gmail.com>
2020-03-06 22:21:38 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+<<<<<<< HEAD
   attr_accessor :username, :password, :balance 
   validates_presence_of :username, :password, :balance
+=======
+  
+>>>>>>> 8cac07d9aa8ae4beee0fa9748f35af42d88fe243
 end
 
--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change 
+=======
+class CreateUsers < ActiveRecord::Migration
+  def change do 
+>>>>>>> 8cac07d9aa8ae4beee0fa9748f35af42d88fe243
     create_table :users do |t| 
     end 
   end 
